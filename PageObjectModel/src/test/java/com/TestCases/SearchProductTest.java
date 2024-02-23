1487cfd8294d28850fb3377c2deb72469555c5cd
BabburiPrasad <babburiprasad97@gmail.com>
2024-02-23 12:31:03 +0530

    Automated NaveenAutomationlabs site with PageObjectModel design flow

---
+++ PageObjectModel/src/test/java/com/TestCases/SearchProductTest.java
@@ -0,0 +1,41 @@
+package com.TestCases;
+
+import org.testng.annotations.Test;
+
+import com.Base.BrowserSetup;
+import com.POM.SearchProductPage;
+
+public class SearchProductTest extends BrowserSetup
+{
+	RegistrationTest reg;
+	LoginTest lgt;
+	SearchProductPage spp;
+	
+	
+	@Test
+	public void searchProduct() throws InterruptedException
+	{
+		lgt = new LoginTest();
+		lgt.login();
+		
+		spp = new SearchProductPage(driver);
+				
+		spp.entersearchInput("Mac");
+		Thread.sleep(1500);
+		
+		spp.clickOnSearch();
+		Thread.sleep(1500);
+		
+		spp.selectProduct();
+		Thread.sleep(1500);
+		
+		spp.addproducttocart();
+		Thread.sleep(1500);
+		
+		System.out.println("Cart Value is: ");
+		spp.getcartvalue();
+		
+		
+		
+	}
+}
